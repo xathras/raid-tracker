8765010766ae15126a39e30d83f89bf88d0d572f
Andrew <akothmann@iexposure.com>
2013-03-07 11:19:01 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaidTracker::Application.config.secret_token = 'f81b78ac35a9cb624542c6a214d961334514ff2dd9125eb6429d6ac1c573e3aa62c230039b43b51a9b61a7b88628ca5135adb5f99e695802a8dc6d14c555057c'
